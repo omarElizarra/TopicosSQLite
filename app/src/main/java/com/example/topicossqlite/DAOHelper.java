363db89ae82017f0c6f51159b5334943df5cfb55
owarup <owarup@gmail.com>
2020-04-09 00:16:23 -0500

    test boton buscar

--- app/src/main/java/com/example/topicossqlite/DAOHelper.java
+++ app/src/main/java/com/example/topicossqlite/DAOHelper.java
@@ -1,6 +1,7 @@
 package com.example.topicossqlite;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -11,7 +12,7 @@ public class DAOHelper extends SQLiteOpenHelper {
     private static String BD = "BD_Test1";
     private static String TABLA = "PERSONAS";
 
-    public DAOHelper(@Nullable Context context,) {
+    public DAOHelper(@Nullable Context context) {
         super(context, BD, null, 1);
     }
 
@@ -24,5 +25,19 @@ public class DAOHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXIST " +TABLA);
+    }
+
+    public Persona buscarPersona (int ID){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor datos = db.rawQuery("SELECT * FROM " +TABLA +" WHERE ID_PER="+ID,null);
+        Persona p =null;
+        if(datos.moveToNext()){
+            p = new Persona();
+            p.setNombre(datos.getString(1));
+            p.setApellido(datos.getString(2));
+            p.setEdad(datos.getInt(3));
+        }
+        db.close();
+        return p;
     }
 }
--- app/src/main/java/com/example/topicossqlite/MainActivity.java
+++ app/src/main/java/com/example/topicossqlite/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.topicossqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DAOHelper d;
     private EditText etID, etNombre, etApellido, etEdad;
     private Button btnBuscar, btnActualizar, btnAgregar, btnEliminar;
     private TextView tvMensaje;
@@ -22,17 +25,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        d = new DAOHelper(this);
+
         etID = (EditText) findViewById(R.id.etID);
         etNombre = (EditText) findViewById(R.id.etNombre);
         etApellido = (EditText) findViewById(R.id.etApellido);
         etEdad = (EditText) findViewById(R.id.etEdad);
 
-        btnBuscar = (Button) = findViewById(R.id.btnBuscar);
-        btnActualizar = (Button) = findViewById(R.id.btnActualizar);
-        btnAgregar = (Button) = findViewById(R.id.btnAgregar);
-        btnEliminar = (Button) = findViewById(R.id.btnEliminar);
+        btnBuscar = (Button)  findViewById(R.id.btnBuscar);
+        btnActualizar = (Button)  findViewById(R.id.btnActualizar);
+        btnAgregar = (Button)  findViewById(R.id.btnAgregar);
+        btnEliminar = (Button)  findViewById(R.id.btnEliminar);
 
-        tvMensaje = (TextView) = findViewById(R.id.tvMensaje);
-        lvDatos = (ListView) = findViewById(R.id.lvDatos);
+        tvMensaje = (TextView)  findViewById(R.id.tvMensaje);
+        lvDatos = (ListView)  findViewById(R.id.lvDatos);
+
+        buscarPersona();
+        estadoArranque();
+    }
+
+    public void estadoArranque(){
+        btnAgregar.setEnabled(false);
+        btnActualizar.setEnabled(false);
+        btnEliminar.setEnabled(false);
+        tvMensaje.setVisibility(View.INVISIBLE);
+        lvDatos.setVisibility(View.INVISIBLE);
+    }
+
+    public void buscarPersona(){
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etID.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this,"Proporsione un numero de ID", Toast.LENGTH_SHORT).show();
+                }else{
+                    int ID = Integer.parseInt(etID.getText().toString());
+                    Persona p = d.buscarPersona(ID);
+                    if (p == null){
+                        Toast.makeText(MainActivity.this,"No se encuentran registros con ese ID. Puede almacenarlo", Toast.LENGTH_SHORT).show();
+                        btnAgregar.setEnabled(true);
+                        btnActualizar.setEnabled(false);
+                        btnEliminar.setEnabled(false);
+                        etNombre.requestFocus();
+                    }else{
+                        etNombre.setText(p.getNombre());
+                        etApellido.setText(p.getApellido());
+                        etEdad.setText(Integer.toString(p.getEdad()));
+                        btnAgregar.setEnabled(false);
+                        btnActualizar.setEnabled(true);
+                        btnEliminar.setEnabled(true);
+                    }
+                }
+
+            }
+        });
     }
 }
